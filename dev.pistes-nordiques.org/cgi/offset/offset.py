6439f3976f721fb6011e648e6c7a91e798942258
yvecai <yvecai@gmail.com>
2012-10-28 16:29:43 +0100

    Multilinestring offset support

--- dev.pistes-nordiques.org/cgi/offset/offset.py
+++ dev.pistes-nordiques.org/cgi/offset/offset.py
@@ -2,12 +2,21 @@
 
 import sys, os
 from shapely.wkt import dumps, loads
+from shapely.geometry import MultiLineString
 
 def handle(req):
     from mod_python import apache, util
     data= req.readline()
-    shape = loads(data)
-    o_object = shape.parallel_offset(0.1, 'left', resolution=1, join_style=1, mitre_limit=1.0)
+    
+	if shape.type == 'MultiLineString':
+		o_lines = []
+		for line in shape.geoms :
+			o_line = line.parallel_offset(0.1, 'left', resolution=1, join_style=1, mitre_limit=1.0)
+			o_lines.append(o_line)
+		o_object = MultiLineString(o_lines).wkt
+		
+	else :	
+		o_object = shape.parallel_offset(0.1, 'left', resolution=1, join_style=1, mitre_limit=1.0)
     
     req.content_type = 'text/plain'
     req.write(o_object.wkt)
