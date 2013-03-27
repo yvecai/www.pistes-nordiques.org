0b7812bfb2f48d67a6ede34584fde4a67b62f187
yvecai <yvecai@gmail.com>
2013-03-27 09:11:39 +0100

    [beta] remove distance calculation (replaced by lonlat distance)

--- www.pistes-nordiques.org/cgi/getProfilePic.py
+++ www.pistes-nordiques.org/cgi/getProfilePic.py
@@ -5,7 +5,7 @@ import Image, ImageDraw, ImageFont
 import sys
 #from lxml import etree
 import math
-from math import floor, ceil
+from math import floor, ceil, sqrt
 import sys, random, re, zipfile
 import StringIO
 
@@ -16,7 +16,8 @@ import os, os.path
 import random
 import atexit
 
-
+SRTMFilesDir='/home/website/SRTM-filled/'
+PIL_images_dir = '/var/www/tmp/' 
 #TODO:
 # Clean and master the elevation code
 
@@ -79,7 +80,6 @@ def createPics(tracks):
 
     randFilename = random.randrange(0, 100001, 2)
     #PIL_images_dir = os.path.join(os.path.dirname(__file__) , 'tmp/')
-    PIL_images_dir = '/var/www/tmp/' 
     profile_filename = 'profile'+str(randFilename)+'.png'
     
     # serialize tracks
@@ -117,14 +117,14 @@ def createPics(tracks):
             if t['lon'] > maxLon: maxLon=t['lon']
     
     # Set markers near the track fourth
-    markers=[]
-    for i in range(1, len(track)):
-        if track[i-1]['dist'] < maxDist/4 and track[i]['dist'] > maxDist/4:
-             markers.append(track[i])
-        if track[i-1]['dist'] < maxDist/2 and track[i]['dist'] > maxDist/2:
-             markers.append(track[i])
-        if track[i-1]['dist'] < maxDist*3/4 and track[i]['dist'] > maxDist*3/4:
-             markers.append(track[i])
+    #~ markers=[]
+    #~ for i in range(1, len(track)):
+        #~ if track[i-1]['dist'] < maxDist/4 and track[i]['dist'] > maxDist/4:
+             #~ markers.append(track[i])
+        #~ if track[i-1]['dist'] < maxDist/2 and track[i]['dist'] > maxDist/2:
+             #~ markers.append(track[i])
+        #~ if track[i-1]['dist'] < maxDist*3/4 and track[i]['dist'] > maxDist*3/4:
+             #~ markers.append(track[i])
              
     # Set the route points
     points=[]
@@ -145,7 +145,7 @@ def createPics(tracks):
     
     sans=ImageFont.truetype(fontfile,10*2)
     #sans=ImageFont.truetype('FreeSans.ttf',10*2) # XX
-    width=230*2
+    width=250*2
     height=120*2
     margin=15
     marginLeft=sans.getsize(str(maxEleValue))[0]
@@ -199,19 +199,19 @@ def createPics(tracks):
     draw.rectangle((0,0,marginLeft,height),\
         fill='#EEEEEE', outline='#EEEEEE')
     # Draw scales:
-    draw.text((margin+marginLeft,height-marginBottom),'0',fill='#000000',font=sans)
-    draw.text((width-sans.getsize(str(maxDistValue))[0],\
-    height-marginBottom),str(maxDistValue),fill='#000000',font=sans)
+    #~ draw.text((margin+marginLeft,height-marginBottom),'0',fill='#000000',font=sans)
+    #~ draw.text((width-sans.getsize(str(maxDistValue))[0],\
+    #~ height-marginBottom),str(maxDistValue),fill='#000000',font=sans)
     draw.text((2,height-marginBottom - sans.getsize('1')[1]/2),minEleValue,fill='#000000',font=sans)
     draw.text((2,\
       height-marginBottom-plotHeight - sans.getsize('1')[1]/2),maxEleValue,fill='#000000',font=sans)
     #Draw markers
-    for m in markers:
-        mDist=" %.1fkm" % m['dist']
-        x=margin+marginLeft+m['dist']/maxDist*plotWidth\
-         -sans.getsize(str(m['dist']))[0]/2
-        y=height-marginBottom
-        draw.text((x,y),mDist,fill='#000000',font=sans)
+    #~ for m in markers:
+        #~ mDist=" %.1fkm" % m['dist']
+        #~ x=margin+marginLeft+m['dist']/maxDist*plotWidth\
+         #~ -sans.getsize(str(m['dist']))[0]/2
+        #~ y=height-marginBottom
+        #~ draw.text((x,y),mDist,fill='#000000',font=sans)
     
     del draw 
     resolution=(int(width/2),int(height/2))
@@ -435,7 +435,7 @@ class SrtmLayer(object):
         Returns the elevation in metres of point (lat, lon).
         """
         srtm_filename = self.get_srtm_filename(lat, lon)
-        SRTMFilesDir='/home/website/SRTM-filled'
+        
         srtm_path = os.path.join(SRTMFilesDir, srtm_filename)
         if not os.path.isfile(srtm_path):
             self._unzip_srtm_tiff(srtm_path)
@@ -484,11 +484,13 @@ def linearDist(lat1, lon1, lat2, lon2):
                   math.cos(lat1)*math.cos(lat2) * \
                   math.cos(lon2-lon1)) * 6371 
     return d
-
 #
 def clamp(value, minvalue, maxvalue):
     return max(minvalue, min(value, maxvalue))
 #
+def llDistance(ll1,ll2):
+    return math.sqrt((ll2['lon']-ll1['lon'])**2+(ll2['lat']-ll1['lat'])**2)
+
 def listTracks(data):
     # split the multiline into a list of line
     lines = re.findall('\([-0-9., ]+\)',data)
@@ -497,11 +499,33 @@ def listTracks(data):
         track=[]
         # split the line into a list of lon lat
         lon_lat = line.replace('(','').replace(')','').split(',')
-        for ll in lon_lat:
-            t={}
-            t['lon']=float(ll.split(' ')[0])
-            t['lat']=float(ll.split(' ')[1])
-            track.append(t)
+        
+        track.append({'lon': float(lon_lat[0].split(' ')[0]), 
+                      'lat':float(lon_lat[0].split(' ')[1])})
+        
+        for i in range(1,len(lon_lat)):
+            t={'lon': float(lon_lat[i].split(' ')[0]), 
+                'lat':float(lon_lat[i].split(' ')[1])}
+            lim=0.02
+            segLength=llDistance(track[i-1],t)
+            if segLength < lim:
+                track.append(t)
+            else:
+                # add a point every 0.002deg at least
+                n = int(segLength/lim)
+                print n
+                lon1=track[-1]['lon']
+                lat1=track[-1]['lat']
+                lon2=t['lon']
+                lat2=t['lat']
+                a = (lat2-lat1)/(lon2-lon1)
+                b= lat1-a*lon1
+                DLON=(lon2-lon1)/float(n)
+                for j in range(0,n+1):
+                    LON=lon1+float(j)*DLON
+                    LAT=a*LON+b
+                    track.append({'lon': LON, 'lat':LAT})
+                    
         tracks.append(track)
     return tracks
 
@@ -516,17 +540,11 @@ def processData(tracks):
             if pt['lat'] == 0 and pt['lon'] ==0 : pt['ele']= 0
             else:
                 pt['ele']=_srtm.get_elevation(float(pt['lat']),float(pt['lon']))
-                if lstEle != 0:
-                    if abs(pt['ele'] - lstEle) > 100: pt['ele']=0 #detect voids
-                    else: lstEle=pt['ele']
-                else: lstEle=pt['ele']
             
         track[0]['dist']=dist
     
         for i in range(1,len(track)):
-                track[i]['dist']=track[i-1]['dist']+\
-                 linearDist(track[i]['lon'],track[i]['lat'],\
-                 track[i-1]['lon'],track[i-1]['lat'])
+                track[i]['dist']=track[i-1]['dist']+llDistance(track[i],track[i-1])
                 dist=track[i]['dist']
         
     return tracks
@@ -564,8 +582,17 @@ def addtionnalComputation(track):
     return None
 #
 if __name__ == "__main__":
-    data=StringIO.StringIO()
-    testFile=open('test_bad.wkt','r')
-    data=testFile.read()
+    SRTMFilesDir=''
+    PIL_images_dir = '/var/tmp/' 
+    f=open(sys.argv[1],'r')
+    data=f.read()
+    tracks=listTracks(data)
+    tracks=processData(tracks)
     print data
-    test(data)
+    for track in tracks:
+        for d in track:
+            if d['lat'] > 71.9999:
+                print 'Sorry, dataset does not contain elevation data beyond 72 deg. latitude'
+    createPics(tracks)
+
+        
